90275f1500a1c7baeb11c798a77363402c523311
Ryan <ryanlee.berkeley@gmail.com>
2018-03-01 16:18:21 -0800

    connect input to cipher function

--- cipher.rb
+++ cipher.rb
@@ -1,3 +1,30 @@
 require 'sinatra'
 
 require 'sinatra/reloader' if development?
+
+def caesar_cipher(sentence, shift)
+
+
+  alphabet = ('a'..'z').to_a
+  sentence.chars.map do |char|
+    index = alphabet.index(char.downcase)
+    if index
+      shifted_char = alphabet[(index + shift)%26]
+      char.downcase == char ? shifted_char : shifted_char.upcase
+    else
+      char
+    end
+  end.join("")
+end
+
+
+
+
+get '/' do
+
+	par = params.to_s
+
+	output = caesar_cipher(params["text"],params["shift"].to_i)
+
+	erb :index, :locals => {:output => output, :par => par}
+end
